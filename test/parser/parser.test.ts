95e08fd82a92eb59a396bdf24ffcba706ee69b86
Gabriel <furstenheim@gmail.com>
2021-10-19 19:49:46 +0200

    Run test

---
+++ test/parser/parser.test.ts
@@ -0,0 +1,18 @@
+import path from 'path'
+import fs from 'fs-extra'
+import * as mocha from 'mocha'
+import * as parser from '../../src/parser/parser'
+import { expect } from 'chai'
+
+const exampleFolder = path.resolve(__dirname, './examples')
+mocha.describe('Check AST', function () {
+  for (const filename of fs.readdirSync(exampleFolder).filter(f => f.endsWith('.input.sql'))) {
+    mocha.it(filename, async function () {
+      const inputFileName = path.join(exampleFolder, filename)
+      const outputFileName = inputFileName.replace('input.sql', 'output.json')
+      const [inputContent, outputContent] = await Promise.all([fs.readFile(inputFileName), fs.readJSON(outputFileName)])
+      const parsed = parser.parseForESLint(inputContent.toString())
+      expect(parsed).equal(outputContent)
+    })
+  }
+})
