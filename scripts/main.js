2bbf7789d7c3725f45633c60ff66862d42f2e59a
Gabriel <gfursten@amazon.com>
2021-11-04 02:06:19 +0100

    Create interface structure in typescript

--- scripts/main.js
+++ scripts/main.js
@@ -13,7 +13,7 @@ async function main () {
   const config = configContent.toString().split('\n')
   const noahdbPath = config[0]
   const files = await fs.readdir(noahdbPath)
-  const goFiles = files.filter(f => f.endsWith('.go')).slice(0, 40)
+  const goFiles = files.filter(f => f.endsWith('.go')).slice(0, 200)
 
   for (const file of goFiles) {
     await parseFile(file)
@@ -26,11 +26,36 @@ async function main () {
       return null
     }
     // console.log(struct[0])
-    const structName = struct[1]
+    const structName = struct[1].replace('_', '')
     const fieldLines = struct[2].split('\n')
-    const fields = _.filter(fieldLines.map(l => l.split(/\s+/)).map(s => {return {name: s[1], type: s[2]}}), 'type')
+    const fields = _.filter(fieldLines.map(l => l.split(/\s+/)).map(s => {
+      if (s[1] && s[1].startsWith('/*')) {
+        return null
+      }
+      return {name: s[1], type: s[2]}}), 'type')
     console.log(fields)
     console.log(structName)
+    const mappings = {
+      int: 'number',
+      List: 'Node[]',
+      bool: 'Boolean'
+    }
+    const typescriptDefinitions = fields.map(function (f) {
+      let type
+      if (mappings[f.type]) {
+        type = mappings[f.type]
+      } else {
+        type = f.type.replaceAll('*', '').replaceAll('_', '')
+      }
+      return `  ${f.name.toLowerCase()}: ${type}`
+    })
+    const classDefinition = `import type {Node} from './Node'
+
+export interface ${structName} {
+${typescriptDefinitions.join('\n')}
+}`
+    console.log(classDefinition)
+    await fs.writeFile(path.join('./parsedClasses', structName + '.ts'), classDefinition)
   }
 }
 
